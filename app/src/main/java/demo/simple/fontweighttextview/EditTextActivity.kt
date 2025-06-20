e5f02273364a863e6c8432997663325b6926ad74
Peng Chen <383559698@qq.com>
2025-06-20 19:31:30 +0800

    update EditTextActivity.kt

--- app/src/main/java/demo/simple/fontweighttextview/EditTextActivity.kt
+++ app/src/main/java/demo/simple/fontweighttextview/EditTextActivity.kt
@@ -1,12 +1,24 @@
 package demo.simple.fontweighttextview
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class EditTextActivity : AppCompatActivity() {
 
+    private val tvAndroid: TextView by lazy { findViewById<TextView>(R.id.tvAndroid) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text)
+        bindAndroidVersion()
+    }
+
+    private fun bindAndroidVersion() {
+        val versionName = Build.VERSION.RELEASE // 获取系统版本名称
+        val sdkInt = Build.VERSION.SDK_INT      // 获取系统API级别
+        val versionText = "Android version: $versionName (API Level: $sdkInt)"
+        tvAndroid.text = versionText
     }
 }
