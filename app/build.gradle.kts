9c17096264f5b51dc56a04aa344e9e1e5738cd83
Peng Chen <383559698@qq.com>
2025-06-27 20:17:52 +0800

    target v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
 //    androidTestImplementation(libs.androidx.junit)
 //    androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(project(":font_weight_textview"))
-//    implementation("com.github.simplepeng:FontWeightTextView:1.0.0")
+//    implementation(project(":font_weight_textview"))
+    implementation("com.github.simplepeng:FontWeightTextView:1.1.0")
 //    implementation("com.github.simplepeng:FontWeightTextView:compat_161")
     implementation("top.canyie.pine:core:0.2.9")
     //
